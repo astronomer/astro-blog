23f57cff939c28c7f0008eb924c1f532b60083ff
sunkickr <davidkoenit@gmail.com>
2020-11-11 14:29:18 -0500

    added index page

--- astro-blog-cms/src/pages/index.js
+++ astro-blog-cms/src/pages/index.js
@@ -1,5 +1,16 @@
 import React from "react"
+import './index.css'
 
 export default function Home() {
-  return <div>Welcome to the Astro Blog CMS. Go to /admin/ to login.</div>
+  return (
+    <body>
+      <header>
+        <image></image>
+        <div className='top'>Astronomer Blog CMS</div>
+      </header>
+      <div className='form'>
+        <a href='/admin/' className='button'>Continue to CMS</a>
+      </div>
+    </body>
+  )
 }
