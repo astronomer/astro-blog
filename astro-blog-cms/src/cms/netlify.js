ac8bf887f90ce007e24357c482521e4e6bdfa806
Samantha Black <sam@astronomer.io>
2021-04-02 10:27:57 -0400

    fix: remove raw body

--- astro-blog-cms/src/cms/netlify.js
+++ astro-blog-cms/src/cms/netlify.js
@@ -21,7 +21,6 @@ const PostPreview = ({ entry, getAsset, widgetsFor }) => (
     <div className={s.body}>
       <div className={s.sidebar}>{' '}</div>
       <Article className={s.postBody}>
-        <div dangerouslySetInnerHTML={{ __html: widgetsFor('body') }} />
         <div>
           {
             unified()
