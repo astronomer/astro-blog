5602b5ea1f3815c37b2ed2ffff48fabee62472cd
medinab <medina1brandon@gmail.com>
2021-04-14 20:29:44 -0400

    see if the config is messing something up

--- astro-blog-cms/gatsby-config.js
+++ astro-blog-cms/gatsby-config.js
@@ -5,13 +5,5 @@
  */
 
  module.exports = {
-  plugins: [
-    {
-      resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/netlify.js`,
-        stylesPath: `${__dirname}/src/pages/index.css`
-      },
-    },
-  ],
+  plugins: ['gatsby-plugin-netlify-cms']
  }
